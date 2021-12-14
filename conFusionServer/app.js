96af6cf7316a93080dac9a6cbcc431030a4c35b7
Anjali <patilanjali1632@gmail.com>
2021-12-14 19:46:47 +0530

    Integrating React Client and Server

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -15,7 +15,7 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
 var favoriteRouter = require("./routes/favoriteRouter");
-
+var commentRouter = require('./routes/commentRouter');
 
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
@@ -67,6 +67,7 @@ app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
 app.use("/favorites", favoriteRouter);
+app.use('/comments',commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- conFusionServer/routes/cors.js
+++ conFusionServer/routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
+var whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://EC2AMAZ-6A95KNP:3001'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
